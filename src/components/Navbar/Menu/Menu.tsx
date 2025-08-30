39040ac754a875a0efd4bcde76814b12753bfb6b
stepan blaha <stepa15.b@gmail.com>
2025-08-30 15:50:00 +0200

    feat: new mobile navbar menu

---
+++ src/components/Navbar/Menu/Menu.tsx
@@ -0,0 +1,98 @@
+import styles from "./Menu.module.css"
+import { useState, useEffect } from 'react';
+import { X, ChevronLeft, ChevronDown } from 'lucide-react';
+
+import { motion, AnimatePresence } from 'framer-motion';
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+/**
+* Props for the Menu component.
+*/ 
+interface MenuProps{
+    display?:boolean, // Whether to display menu
+    handleDisplay: (val: boolean) => void, // Display change event handler
+    version?: "onboarding" | "default" | "settings" // Version of the menu layout
+}
+/**
+* A menu component with customizable display and layout versions.
+* Contains various links, language and currency swittch and a logout option
+*
+* @param props Menu props
+* @param {boolean} props.display - Whether to display menu
+* @param {(val: boolean) => void} props.handleDisplay - Display change event handler
+* @param {"onboarding" | "default" | "settings"} props.version - Version of the menu layout
+* @returns TSX.Element
+*/
+const Menu = ({display = false, handleDisplay, version = "default"} : MenuProps) => {
+    const { t } = useTranslation(); // Translation
+
+    const navigate = useNavigate(); // Navigate
+    // Flags for opening and closing the modals
+    const [langOpen, setLangOpen] = useState<boolean>(false);
+    const [currOpen, setCurrOpen] = useState<boolean>(false);
+    const [userOpen, setUserOpen] = useState<boolean>(false);
+    const [logoutOpen, setLogoutOpen] = useState<boolean>(false)
+
+
+    return(
+        <>
+
+        <AnimatePresence>
+            {display === true && (
+            <motion.div 
+                initial={{ x: '100%' }}
+                animate={{ x: 0 }}
+                exit={{ x: '100%' }}
+                transition={{ duration: 0.4, ease: 'easeInOut' }}
+                className={styles.MenuMask}>
+            <div className={styles.Menu} >
+                <div className={styles.Wrapper}>
+                    <div className={styles.TitlePart}>
+                        <p>SBSORT</p>
+                        <X onClick={()=>handleDisplay(false)}/>
+                    </div>
+                    <div className={styles.ContentPart}>
+                        <>
+                            <div className={styles.LinkGroup}>
+                                <div className={styles.MenuItem}>
+                                    <button className={styles.MenuItemHeader} onClick={()=>navigate("/home")}>
+                                        <ChevronLeft/>
+                                        <p>Home</p>
+                                    </button>
+                                </div>
+                                <div className={styles.MenuItem}>
+                                    <button className={styles.MenuItemHeader} onClick={()=>navigate("/list")}>
+                                        <ChevronLeft/>
+                                        <p>Sort Types</p>
+                                    </button>
+                                </div>
+                                <div className={styles.MenuItem}>
+                                    <button className={styles.MenuItemHeader} onClick={()=>navigate("/about")}>
+                                        <ChevronLeft/>
+                                        <p>About</p>
+                                    </button>
+                                </div>
+                            </div>
+                            <div className={styles.LinkGroup}>
+                                <div className={styles.MenuItem}>
+                                    <button className={styles.MenuItemHeader} onClick={()=>setLangOpen(!langOpen)}>
+                                        {langOpen === true ? <ChevronDown/> : <ChevronLeft/>}
+                                        <p>{t("navbar.language")}</p>
+                                    </button>
+                                    {/*langOpen === true && (
+                                        <LanguageSwitch type="mobile"/>
+                                    ) */}
+                                </div>
+                            </div>
+                        </>
+                    </div>
+                </div>
+               
+                </div>
+            </motion.div>
+            )}
+            </AnimatePresence>
+        </>
+    )
+};
+export default Menu;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,6 +8,115 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useNavigate } from "react-router-dom"
 import { useTheme } from "../../context/ThemeContext"
 
+import MenuBlock from "./Menu/Menu"
+
+
+const Navbar = () => {
+    const navigate = useNavigate();
+    const { theme } = useTheme()
+    const width = useWindowSize();
+    const [ menuOpen, setMenuOpen] = useState<boolean>(false)
+    const [activeId, setActiveId] = useState<string>("home");
+
+    useEffect(()=>{
+        if(width > 600 && menuOpen === true){
+            setMenuOpen(false)
+        }
+    }, [width])
+    
+    // Handle clickoff
+    const ref = useRef<HTMLDivElement>(null); // Ref for the dropdown
+    const buttonRef = useRef<HTMLDivElement>(null); // Ref for the dropdown button
+    // Handle click outside
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (ref.current && !ref.current.contains(event.target as Node) && buttonRef.current && !buttonRef.current.contains(event.target as Node)) {
+                setMenuOpen(false);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
+    
+    const sections = ["Home", "Sort Types", "About"]
+
+
+    return (
+        <>
+            <MenuBlock display={menuOpen} handleDisplay={setMenuOpen} version="default"/>
+        {width > 800 ? (
+            <div className={styles.Header}>
+                <div className={styles.Logo}
+                >
+                    <img src={theme === "light" ? logo : darklogo} alt="SBSORT logo" />
+                </div>
+                <div className={styles.HeaderGroup}>
+
+                    <div className={styles.Pages}>
+                        <div className={styles.HeaderPage} onClick={()=>navigate("/home")}>
+                            <a>Home</a>
+                        </div>
+                        <div className={styles.HeaderPage} onClick={()=>navigate("/list")}>
+                            <a>Sort Types</a>
+                        </div>
+                        <div className={styles.HeaderPage} onClick={()=>navigate("/about")}>
+                            <a>About</a>
+                        </div>
+                    </div>
+                    <div className={styles.Theme}>
+                        <ThemeSwitch/>
+                    </div>
+                </div>
+            </div>
+            ) : (
+            <div className={styles.Header}>
+                <div className={styles.Logo}>
+                    <img src={theme === "light" ? logo : darklogo} alt="SBSORT logo" />
+                </div>
+            <div className={styles.NavbarMobile} >
+                <div className={styles.NavbarHeader} ref={buttonRef} >
+                    <label className={`${styles.hamburger} ${menuOpen ? styles.inputChecked : ''}`}>
+                        <input type="checkbox" checked={menuOpen} onChange={()=>setMenuOpen(!menuOpen)} />
+                        <svg viewBox="0 0 32 32">
+                            <path
+                            className={`${styles.line} ${styles.lineTopBottom}`}
+                            d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
+                            />
+                            <path className={styles.line} d="M7 16 27 16" />
+                        </svg>
+                    </label>
+                </div>
+                <ThemeSwitch/>
+                
+            </div>
+        </div>
+        )}
+        </>
+    )
+}
+
+export default Navbar;
+
+
+
+
+
+
+
+
+
+
+/**
+import styles from "./Navbar.module.css"
+import logo from "../../assets/logos/SBSORT-light.png"
+import darklogo from "../../assets/logos/SBSORT-dark.png"
+import ThemeSwitch from "../ThemeSwitch/ThemeSwitch"
+import { useState, useEffect, useRef } from "react"
+import { useWindowSize } from "../../hooks/useWindowSize"
+import { motion, AnimatePresence } from "framer-motion";
+import { useNavigate } from "react-router-dom"
+import { useTheme } from "../../context/ThemeContext"
+
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -128,3 +237,4 @@ const Navbar = () => {
 }
 
 export default Navbar;
+ */
